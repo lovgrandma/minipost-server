02cd4f8e482e4b8e62ac05d11527602a74c143f0
Jesse <jessealexanderthompson@gmail.com>
2020-05-28 22:50:13 -0400

    dummy s3 cred files

---
+++ routes/api/s3credentialsdummy.js
@@ -0,0 +1,16 @@
+// This is what your s3credentials.js file should look like. Place in this same directory.
+
+exports = module.exports = {
+    awsConfig: {
+        accessKeyId: 'XXXXXXXXXXXXXXXXXXX',
+        secretAccessKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+        region:'us-east-2'
+    },
+    cloudFrontKeysPath: {
+        public: "./routes/api/keys/rsa-XXXXXXXXXXXXXXX.pem",
+        private: "./routes/api/keys/pk-XXXXXXXXXXXXXXXXXXX.pem"
+    },
+    cdn: {
+        cloudFront1: "https://XXXXXXXXXXXXXXX.cloudfront.net"
+    }
+};
